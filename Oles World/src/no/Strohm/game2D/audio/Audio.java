3f04d540c8f5ae30b86dc9b40eecf5a14632a638
Ole Strohm <olestr@grabit.no>
2015-03-10 21:36:59 +0100

    loop, play

--- Oles World/src/no/Strohm/game2D/audio/Audio.java
+++ Oles World/src/no/Strohm/game2D/audio/Audio.java
@@ -30,6 +30,23 @@ public class Audio {
 					clip.open(inputStream);
 					FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 					gainControl.setValue(-20f);
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}).start();
+	}
+
+	public void loop() {
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+							Game.class.getResourceAsStream("/audio/" + fileName));
+					clip.open(inputStream);
+					FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+					gainControl.setValue(-20f);
 					clip.loop(Clip.LOOP_CONTINUOUSLY);
 				} catch (Exception e) {
 					System.err.println(e.getMessage());
